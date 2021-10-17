6555067135aac3f19df2b9fb21acf55a89dd9a72
Agentew04 <digoappelt@gmail.com>
2021-10-17 19:12:03 -0300

    add command exceptions

---
+++ TwitchPointsFarmer/Utils/Exceptions/TooManyArgsException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace TwitchPointsFarmer.Utils.Exceptions
+{
+    [Serializable]
+    internal class TooManyArgsException : Exception
+    {
+        public TooManyArgsException()
+        {
+        }
+
+        public TooManyArgsException(string message) : base(message)
+        {
+        }
+
+        public TooManyArgsException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected TooManyArgsException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
